36b0a93bfa0e1fc26e179b8d4c4f96009e43cb5d
Victor Yuan <victor.2wy@gmail.com>
2025-07-11 21:47:34 -0700

    alter home text

--- src/home.js
+++ src/home.js
@@ -1,22 +1,22 @@
 import clearContent from './clearContent.js'
 
-function pageLoad() {
+function loadHome() {
   clearContent();
-  
+
   const content = document.getElementById('content');
   const headline = document.createElement('div');
   headline.classList.add('headline');
 
   const headline_h1 = document.createElement('h1');
-  headline_h1.textContent = 'A fake tonkatsu restaurant';
+  headline_h1.textContent = 'The best UNAGI-DON restaurant';
   headline.appendChild(headline_h1);
 
   const headline_p = document.createElement('p');
-  headline_p.textContent = 'SAKU Katsu is a sophisticated restaurant offering authentic katsu in a modern ambiance, providing guests with a meaningful dining experience. Furthermore, we partner with local farms to prioritize ethical practices and animal welfare, ensuring that our dishes are crafted with premium ingredients.';
+  headline_p.textContent = 'KA-DON is an elegant restaurant offering original unagi-DON in a modern ambiance, providing customers with a insightful dining experience. We partner with local farmers and ethical practices with animal welfare, ensuring that our dishes are conceived with high-quality ingredients.';
   headline.appendChild(headline_p);
 
   content.appendChild(headline);
 
 }
 
-export default pageLoad
+export default loadHome
