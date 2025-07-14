581b493fa4d7a002283558470c98f760cb698823
Victor Yuan <victor.2wy@gmail.com>
2025-07-13 20:08:34 -0700

    separate out config files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,29 +1,10 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.common.js');
 
-module.exports = {
+module.exports = merge(common, {
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      }
-    ]
-  }
-}
+});
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,29 +1,7 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.common.js');
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      }
-    ]
-  }
-}
+
+module.exports = merge(common, {
+  mode: "production"
+})
